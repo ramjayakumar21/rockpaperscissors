126fdc7f9d3d0ab44976b6a647cdf6c0f227b8d3
ramjayakumar21 <ramjayakumar21@gmail.com>
2022-07-05 21:31:53 -0700

    Add external CSS file

--- code.js
+++ code.js
@@ -25,6 +25,4 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
